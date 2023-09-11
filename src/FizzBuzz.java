50063bb0b18e7651d8dfb7c3ac52249f6552c76f
Kenta Ploch <kentsploch@Kentas-MacBook-Pro.local>
2023-09-11 14:53:05 -0400

    new stuff

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -16,7 +16,6 @@ class FizzBuzz {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
-        i += 1;
 
         // Print our appropriate result.
         if (divisibleBy3 && divisibleBy5) {
@@ -33,9 +32,10 @@ class FizzBuzz {
 
         } else {
 
-            System.out.println(i -1);
+            System.out.println(i);
 
         }
+        i += 1;
         return i;
     }
 }
